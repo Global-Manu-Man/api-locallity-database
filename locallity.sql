050c7fe21b6006bc07d2d7a5d5cbd1325a82f2b8
Emmanuel Sandoval <84020431+Global-Manu-Man@users.noreply.github.com>
2023-06-05 00:45:59 -0600

    Update locallity.sql
    update databases

--- locallity.sql
+++ locallity.sql
@@ -20,7 +20,12 @@ SET time_zone = "+00:00";
 --
 -- Database: `locallity`
 --
+CREATE DATABASE locallity;
+use locallity;
 
+CREATE USER 'mysqluser'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+GRANT ALL PRIVILEGES ON locallity.* TO 'mysqluser'@'%';
+FLUSH PRIVILEGES;
 -- --------------------------------------------------------
 
 --
