9ca3c95cebb3953a4505fb28c79af836fe180592
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-06-10 20:48:51 +0600

    Updated DB

--- locallity.sql
+++ locallity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2023 at 06:17 PM
+-- Generation Time: Jun 10, 2023 at 04:48 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -20,12 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `locallity`
 --
-CREATE DATABASE locallity;
-use locallity;
 
-CREATE USER 'mysqluser'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
-GRANT ALL PRIVILEGES ON locallity.* TO 'mysqluser'@'%';
-FLUSH PRIVILEGES;
 -- --------------------------------------------------------
 
 --
@@ -55,7 +50,7 @@ INSERT INTO `discount` (`discount_code`, `description`, `date_validity`, `create
 
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
-  `images_id` varchar(10) NOT NULL,
+  `id_business` varchar(10) NOT NULL,
   `image_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -63,7 +58,7 @@ CREATE TABLE `images` (
 -- Dumping data for table `images`
 --
 
-INSERT INTO `images` (`id`, `images_id`, `image_url`) VALUES
+INSERT INTO `images` (`id`, `id_business`, `image_url`) VALUES
 (30, '03e186f4-d', 'https://locallity.s3.us-west-1.amazonaws.com/1686240674293.jpg'),
 (31, '03e186f4-d', 'https://locallity.s3.us-west-1.amazonaws.com/1686240674332.jpg'),
 (32, '03e186f4-d', 'https://locallity.s3.us-west-1.amazonaws.com/1686240674329.jpg'),
@@ -79,7 +74,21 @@ INSERT INTO `images` (`id`, `images_id`, `image_url`) VALUES
 (42, '01fa7bfb-9', 'https://locallity.s3.us-west-1.amazonaws.com/1686240911727.jpg'),
 (43, '01fa7bfb-9', 'https://locallity.s3.us-west-1.amazonaws.com/1686240911767.jpg'),
 (44, '01fa7bfb-9', 'https://locallity.s3.us-west-1.amazonaws.com/1686240911772.jpg'),
-(45, '01fa7bfb-9', 'https://locallity.s3.us-west-1.amazonaws.com/1686240911760.jpg');
+(45, '01fa7bfb-9', 'https://locallity.s3.us-west-1.amazonaws.com/1686240911760.jpg'),
+(46, '5d13225c-a', 'https://locallity.s3.us-west-1.amazonaws.com/1686243380050.jpg'),
+(47, '5d13225c-a', 'https://locallity.s3.us-west-1.amazonaws.com/1686243380021.jpg'),
+(48, '5d13225c-a', 'https://locallity.s3.us-west-1.amazonaws.com/1686243380047.jpg'),
+(49, '5d13225c-a', 'https://locallity.s3.us-west-1.amazonaws.com/1686243380044.jpg'),
+(50, '03422eec-e', 'https://locallity.s3.us-west-1.amazonaws.com/1686243433644.jpg'),
+(51, '03422eec-e', 'https://locallity.s3.us-west-1.amazonaws.com/1686243433637.jpg'),
+(52, '03422eec-e', 'https://locallity.s3.us-west-1.amazonaws.com/1686243433639.jpg'),
+(53, '03422eec-e', 'https://locallity.s3.us-west-1.amazonaws.com/1686243433642.jpg'),
+(54, '3ee633d9-f', 'https://locallity.s3.us-west-1.amazonaws.com/1686314439677.jpg'),
+(55, '3ee633d9-f', 'https://locallity.s3.us-west-1.amazonaws.com/1686314439582.jpg'),
+(56, '3ee633d9-f', 'https://locallity.s3.us-west-1.amazonaws.com/1686314439670.jpg'),
+(57, 'cefe1ea8-1', 'https://locallity.s3.us-west-1.amazonaws.com/1686314514657.jpg'),
+(58, 'cefe1ea8-1', 'https://locallity.s3.us-west-1.amazonaws.com/1686314514633.jpg'),
+(59, 'cefe1ea8-1', 'https://locallity.s3.us-west-1.amazonaws.com/1686314514654.jpg');
 
 -- --------------------------------------------------------
 
@@ -89,19 +98,22 @@ INSERT INTO `images` (`id`, `images_id`, `image_url`) VALUES
 
 CREATE TABLE `logos` (
   `id` int(11) NOT NULL,
-  `logo_id` varchar(10) NOT NULL,
-  `logo_url` varchar(255) NOT NULL
+  `id_business` varchar(10) DEFAULT NULL,
+  `logo_url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `logos`
 --
 
-INSERT INTO `logos` (`id`, `logo_id`, `logo_url`) VALUES
+INSERT INTO `logos` (`id`, `id_business`, `logo_url`) VALUES
 (8, '03e186f4-d', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686240674337.png'),
 (9, '09acd8e5-5', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686240736861.png'),
 (10, '4a29b557-c', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686240754747.png'),
-(11, '01fa7bfb-9', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686240911775.png');
+(11, '01fa7bfb-9', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686240911775.png'),
+(12, '5d13225c-a', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686243380053.png'),
+(13, '03422eec-e', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686243433649.png'),
+(14, 'cefe1ea8-1', 'https://locallity-logos.s3.us-west-1.amazonaws.com/1686314514659.png');
 
 -- --------------------------------------------------------
 
@@ -110,7 +122,7 @@ INSERT INTO `logos` (`id`, `logo_id`, `logo_url`) VALUES
 --
 
 CREATE TABLE `negocio` (
-  `business_id` varchar(11) NOT NULL,
+  `id_business` varchar(11) NOT NULL,
   `schedule` varchar(100) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
@@ -131,8 +143,8 @@ CREATE TABLE `negocio` (
   `physical_store` tinyint(1) DEFAULT NULL,
   `online_store` tinyint(1) DEFAULT NULL,
   `url_google` varchar(100) DEFAULT NULL,
-  `business_age` varchar(50) NOT NULL,
-  `municipality` varchar(50) NOT NULL,
+  `business_age` varchar(50) DEFAULT NULL,
+  `municipality` varchar(50) DEFAULT NULL,
   `cell_phone_number` varchar(20) DEFAULT NULL,
   `business_days` varchar(100) DEFAULT NULL,
   `category` varchar(100) DEFAULT NULL,
@@ -154,11 +166,15 @@ CREATE TABLE `negocio` (
 -- Dumping data for table `negocio`
 --
 
-INSERT INTO `negocio` (`business_id`, `schedule`, `name`, `price`, `manager`, `description`, `address_1`, `address_2`, `address_3`, `city`, `state`, `country`, `postal_code`, `delivery`, `shipping`, `bill`, `antiquity`, `email`, `physical_store`, `online_store`, `url_google`, `business_age`, `municipality`, `cell_phone_number`, `business_days`, `category`, `subcategory`, `discount_code`, `publication_likes`, `questions`, `policies_terms`, `accepts_credit_cards`, `is_owner_verified`, `social_networks`, `status`, `created_at`, `start_date`, `end_date`) VALUES
-('01fa7bfb-9', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '1-5', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', 'Subcategoría', 'DESC10', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:15:11', '2023-05-26', '2023-06-30'),
+INSERT INTO `negocio` (`id_business`, `schedule`, `name`, `price`, `manager`, `description`, `address_1`, `address_2`, `address_3`, `city`, `state`, `country`, `postal_code`, `delivery`, `shipping`, `bill`, `antiquity`, `email`, `physical_store`, `online_store`, `url_google`, `business_age`, `municipality`, `cell_phone_number`, `business_days`, `category`, `subcategory`, `discount_code`, `publication_likes`, `questions`, `policies_terms`, `accepts_credit_cards`, `is_owner_verified`, `social_networks`, `status`, `created_at`, `start_date`, `end_date`) VALUES
+('01fa7bfb-9', '6:00 AM-6:00 AM', 'Update data', '12000.00', 'Update item', '', '', '', '', '', '', '', '', 0, 0, 0, 0, '', 0, 0, '', '10', 'municipality', '', '', '', '', '', 0, 0, '', 0, 0, '', '', '2023-06-08 16:15:11', '2023-05-26', '2023-06-30'),
+('03422eec-e', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', '', '', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:57:13', '2023-05-26', '2023-06-30'),
 ('03e186f4-d', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '1-5', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', 'Subcategoría', 'DESC10', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:11:14', '2023-05-26', '2023-06-30'),
 ('09acd8e5-5', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '1-5', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', 'Subcategoría', 'DESC10', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:12:16', '2023-05-26', '2023-06-30'),
-('4a29b557-c', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '1-5', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', 'Subcategoría', 'DESC10', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:12:34', '2023-05-26', '2023-06-30');
+('3ee633d9-f', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', '', '', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-09 12:40:39', '2023-05-26', '2023-06-30'),
+('4a29b557-c', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '1-5', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', 'Subcategoría', 'DESC10', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:12:34', '2023-05-26', '2023-06-30'),
+('5d13225c-a', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', 'undefined', 'undefined', '555-123-4567', 'Lunes a Viernes', 'Categoría', 'undefined', 'undefined', 100, 5, 'policies and terms', 0, 0, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-08 16:56:20', '2023-05-26', '2023-06-30'),
+('cefe1ea8-1', '6:00 AM-6:00 AM', 'Muhammad Rafi', '10000.00', 'Muhammad Rafi', 'A place to celebrate in a big way', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Juarez Ave. #123', 'Mexico City', 'Mexico City', 'Mexico ', '4100', 1, 1, 1, 5, 'info@minegocio.com', 1, 1, 'https://goo.gl/maps/vbdhqb5JgeV84Tyr8', '', 'demo', '555-123-4567', 'Lunes a Viernes', 'Categoría', '', '', 100, 5, 'policies and terms', 1, 1, 'https://facebook.com/minegocio,https://twitter.com/minegocio,https://instagram.com/minegocio', 'Activo', '2023-06-09 12:41:54', '2023-05-26', '2023-06-30');
 
 --
 -- Indexes for dumped tables
@@ -175,20 +191,20 @@ ALTER TABLE `discount`
 --
 ALTER TABLE `images`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `images_id` (`images_id`);
+  ADD KEY `images_id` (`id_business`);
 
 --
 -- Indexes for table `logos`
 --
 ALTER TABLE `logos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `logo_id` (`logo_id`);
+  ADD KEY `logo_id` (`id_business`);
 
 --
 -- Indexes for table `negocio`
 --
 ALTER TABLE `negocio`
-  ADD PRIMARY KEY (`business_id`);
+  ADD PRIMARY KEY (`id_business`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -198,13 +214,13 @@ ALTER TABLE `negocio`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `logos`
 --
 ALTER TABLE `logos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -214,13 +230,13 @@ ALTER TABLE `logos`
 -- Constraints for table `images`
 --
 ALTER TABLE `images`
-  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`images_id`) REFERENCES `negocio` (`business_id`);
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id_business`) REFERENCES `negocio` (`id_business`);
 
 --
 -- Constraints for table `logos`
 --
 ALTER TABLE `logos`
-  ADD CONSTRAINT `logos_ibfk_1` FOREIGN KEY (`logo_id`) REFERENCES `negocio` (`business_id`);
+  ADD CONSTRAINT `logos_ibfk_1` FOREIGN KEY (`id_business`) REFERENCES `negocio` (`id_business`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
